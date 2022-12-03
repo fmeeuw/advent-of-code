36fc1b84e2e7e703f22a65569152ebdabb1caf82
Frank van Meeuwen <vanmeeuwen@zorgdomein.nl>
2022-12-03 09:18:55 +0100

    day3

---
+++ src/main/scala/aoc2022/Day3.scala
@@ -0,0 +1,44 @@
+package aoc2022
+
+import util.PuzzleInputOps
+
+import scala.io.Source
+
+object Day3 extends App {
+
+  case class Rucksack(part1: String, part2: String)
+
+  def part1 = parseInput.map { sack =>
+    val intersectionChars = sack.part1.toList.toSet.intersect(sack.part2.toList.toSet)
+    priority(intersectionChars.toList.head)
+  }.sum
+
+  def part2 = parseInput
+    .map(sack => (sack.part1 ++ sack.part2).toSet)
+    .grouped(3)
+    .map { group =>
+      val badge = group.reduceLeft((a, b) => a.intersect(b)).head
+      priority(badge)
+    }
+    .sum
+
+  private def priority(char: Char) = {
+    if (char.isUpper) {
+      char.toInt - 38
+    } else {
+      char.toInt - 96
+    }
+  }
+
+  def parseInput: List[Rucksack] = {
+    PuzzleInputOps
+      .readLines(2022, 3)
+      .map { line =>
+        Rucksack(line.take(line.length / 2), line.drop(line.length / 2))
+      }
+      .toList
+  }
+
+  println(part1) // 7848
+  println(part2) // 2616
+}
